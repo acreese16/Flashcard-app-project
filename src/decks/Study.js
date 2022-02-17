8658a588a3891c8a6c052c31ac74522b96769ea5
Ashley Creese <creese.ashley@gmail.com>
2022-02-16 19:40:42 -0500

    Commiting changes to Study Component

--- src/decks/Study.js
+++ src/decks/Study.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useParams, useHistory, Link } from "react-router-dom";
 import { readDeck, readCard } from "../utils/api/index";
 
+
 function Study() {
   //create a variable for the initial state of the deck to be an object
   const [deck, setDeck] = useState({});
@@ -11,7 +12,7 @@ function Study() {
   const [activeCard, setActiveCard] = useState(0);
   // to set the card to be facing the front side of the card first
   const [front, setFront] = useState(true);
-  const [cards, setCards] = useState([]);
+  // const [cards, setCards] = useState([]);
   const history = useHistory();
 
   // useEffect to get the proper deck
@@ -20,22 +21,31 @@ function Study() {
     const abortControl = new AbortController();
 
     const getDeck = async () => {
-      const activeDeck = await readDeck(deckId);
+      const activeDeck = await readDeck(deckId, abortControl.signal);
+      console.log(activeDeck);
       setDeck(() => activeDeck);
     };
     getDeck();
     return () => abortControl.abort();
   }, [deckId]);
 
+  // declaring function which is invoked by the [cardId]
   useEffect(() => {
+    
     const abortControl = new AbortController();
 
+    // variable calling a async function 
     const cardAPIInfo = async () => {
       const response = await readCard(cardId, abortControl.signal);
-      setCards(() => response);
+      console.log(response);
+      setActiveCard(() => response);
     };
-    cardAPIInfo();
-
+    // if there is a cardId then invoke the function 
+    if (cardId) {
+     cardAPIInfo(); // invoking the function
+    };
+  
+    
     return () => {
       abortControl.abort();
     };
@@ -43,7 +53,7 @@ function Study() {
 
   const nextCardHandler = () => {
     // if the current card is the last card in the deck
-    if (activeCard === cards.length - 1) {
+    if (activeCard === deck.cards.length - 1) {
       // then ask if user wishes to restart or continue to home screen, if OK then restart cards, if cancel go home
       window.confirm(
         "Restart cards?\nClick 'Cancel' to return to the home page."
@@ -58,11 +68,20 @@ function Study() {
   };
 
   const flippingCardHandler = () => {
-    setFront(() => !front);
+    console.log("Hello World");
+    setFront(() => !front)
   };
 
+  /*   if (deck.cards.length >= 3) {
+    return (
+        <p>There are {deck.cards.length} cards in this deck</p>
+    )
+  } else {
+    return <p>Whoops, try again</p>
+  } */
+
   // if the deck exists and the amount of cards is greater than 2 then render
-  if (cards.length > 2) {
+  if (deck && deck.cards && deck.cards.length >= 3) {
     return (
       <div>
         <nav aria-label="breadcrumb">
@@ -79,52 +98,35 @@ function Study() {
           </ol>
         </nav>
         <h2 className="d-flex">{deck.name}: Study</h2>
-        <div className="card-body">
-          <h5 className="card-title">
-            Card {activeCard + 1} of {cards.length}
-          </h5>
-          <p className="card-text">
-            {front ? cards[activeCard].front : cards[activeCard].back}
-          </p>
-          <button
-            className="btn btn-secondary mr-3"
-            onClick={flippingCardHandler}
-          >
-            Flip
-          </button>
-          {front ? null : (
-            <button className="btn btn-primary" onClick={nextCardHandler}>
-              Next
+        <div className="card">
+          <div className="card-body">
+            <h5 className="card-title">
+              Card {activeCard + 1} of {deck.cards.length}
+            </h5>
+            <p className="card-text">
+              {front
+                ? deck.cards[activeCard].front
+                : deck.cards[activeCard].back}
+            </p>
+            <button
+              className="btn btn-secondary mr-3"
+              onClick={flippingCardHandler}
+            >
+              Flip
             </button>
-          )}
+            {front ? null : (
+              <button className="btn btn-primary" onClick={nextCardHandler}>
+                Next
+              </button>
+            )}
+          </div>
         </div>
       </div>
     );
   } else {
     return (
       <div>
-        <nav aria-label="breadcrumb">
-          <ol className="breadcrumb">
-            <li className="breadcrumb-item">
-              <Link to={"/"}>Home</Link>
-            </li>
-            <li className="breadcrumb-item">
-              <Link to={`/decks/${deckId}`}>{deck.name}</Link>
-            </li>
-            <li className="breadcrumb-item active" aria-label="page">
-              Study
-            </li>
-          </ol>
-        </nav>
-        <h2 className="d-flex">{deck.name}: Study</h2>
-        <h3>Not enough cards.</h3>
-        <p>
-          You need at least 3 cards to study. There are {cards.length} cards in
-          this deck.
-        </p>
-        <Link to={`/decks/${deckId}/cards/new`} className="btn btn-primary">
-          + Add Card
-        </Link>
+        <p>Loading</p>
       </div>
     );
   }
